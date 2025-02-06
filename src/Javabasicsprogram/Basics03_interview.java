249a6a25fb956d44049cc3cb3d034658ab5363af
Your Name <you@example.com>
2025-02-06 23:11:53 +0530

    commitjava

---
+++ src/Javabasicsprogram/Basics03_interview.java
@@ -0,0 +1,9 @@
+package Javabasicsprogram;
+
+public class Basics03_interview {
+    public static void main(String[] args) {
+        int a_b =78;
+   int x=a_b;
+        System.out.println(x);
+    }
+}
