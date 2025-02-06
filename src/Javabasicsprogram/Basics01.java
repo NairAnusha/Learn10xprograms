310a2950bd0b75e7d02844354614d00aaeaa9c52
Your Name <you@example.com>
2025-02-06 23:12:46 +0530

    commitjava

--- src/Javabasicsprogram/Basics01.java
+++ src/Javabasicsprogram/Basics01.java
@@ -1,6 +1,6 @@
 package Javabasicsprogram;
 
-public class javatest {
+public class Basics01 {
     public static void main(String[] args)
     {
         System.out.println("this is a test program0");
